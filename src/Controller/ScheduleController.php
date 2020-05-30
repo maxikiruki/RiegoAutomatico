78832f9da27b2e9365a5bdb8f7530794a9241983
Jaime <jaimenavarrol97@gmail.com>
2020-05-30 19:17:33 +0200

    crear formulario

--- src/Controller/ScheduleController.php
+++ src/Controller/ScheduleController.php
@@ -39,6 +39,21 @@ class ScheduleController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             //COMPROBAR DIFERENCIA DE HORAS
+            
+            $init_morning=$_REQUEST['start_time_morning'];
+            
+            $final_morning=$_REQUEST[`end_time_morning`];
+            
+
+            if(($init_morning <= $_final_morning) ){
+                //return $this->redirectToRoute('schedule_new');
+                return $this->render('schedule/new.html.twig', [
+                    'schedule' => $schedule,
+                    'form' => $form->createView(),
+                ]);
+            }
+            
+
 
             $entityManager = $this->getDoctrine()->getManager();
             $schedule->setVisible(false);
